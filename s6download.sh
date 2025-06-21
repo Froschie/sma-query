1fe3e4ca2b96aa2faef03214b447724e06e1bcab
Froschie <github@froschie.de>
2025-06-21 22:23:23 +0200

    use old s6overlay

--- s6download.sh
+++ s6download.sh
@@ -5,33 +5,33 @@ then
     if [ "${arch}" == 'x86_64' ];
     then
     echo "X64 Architecture"
-    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-amd64.tar.xz
+    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-amd64.tar.gz
     fi
     if [ "${arch}" == 'armv7l' ];
     then
     echo "Arm architecture"
-    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-armhf.tar.xz
+    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-armhf.tar.gz
     fi
     if [ "${arch}" == 'aarch64' ];
     then
     echo "Arm64 architecture"
-    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-aarch64.tar.xz
+    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-aarch64.tar.gz
     fi
 else
     echo "Platform: "${TARGETPLATFORM}
     if [ "${TARGETPLATFORM}" == 'linux/amd64' ];
     then
     echo "X64 Architecture"
-    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-amd64.tar.xz
+    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-amd64.tar.gz
     fi
     if [ "${TARGETPLATFORM}" == 'linux/arm/v7' ];
     then
     echo "Arm architecture"
-    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-armhf.tar.xz
+    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-armhf.tar.gz
     fi
     if [ "${TARGETPLATFORM}" == 'linux/arm64' ];
     then
     echo "Arm64 architecture"
-    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/latest/download/s6-overlay-aarch64.tar.xz
+    curl -o /tmp/s6overlay.tar.xz -L https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-aarch64.tar.gz
     fi
 fi
